83eea97abef1bf11eb0ec280c07512d00802dd05
珠音茉白 <80902436+armadillo-winX@users.noreply.github.com>
2026-05-16 23:56:55 +0900

    AddSubjectDialog 追加

---
+++ FsReport.Desktop/Dialogs/AddSubjectDialog.xaml.cs
@@ -0,0 +1,32 @@
+﻿namespace FsReport.Desktop.Dialogs
+{
+    /// <summary>
+    /// AddSubjectDialog.xaml の相互作用ロジック
+    /// </summary>
+    public partial class AddSubjectDialog : Window
+    {
+        public string? SubjectName { get; set; }
+
+        public string? SubjectFolderName { get; set; }
+
+        public AddSubjectDialog()
+        {
+            InitializeComponent();
+        }
+
+        private void AddButtonClick(object sender, RoutedEventArgs e)
+        {
+            if (SubjectNameBox.Text.Length > 0 && SubjectFolderNameBox.Text.Length > 0)
+            {
+                this.SubjectName = SubjectNameBox.Text;
+                this.SubjectFolderName = SubjectFolderNameBox.Text;
+                this.DialogResult = true;
+            }
+            else
+            {
+                MessageBox.Show(this, "科目名か科目名に割り当てるフォルダ名を入力してください．",
+                    VersionInfo.Name, MessageBoxButton.OK, MessageBoxImage.Exclamation);
+            }
+        }
+    }
+}
