b20dd4e8de243e0b3d837736ea0f5320874e5b4b
珠音茉白 <80902436+armadillo-winX@users.noreply.github.com>
2026-05-17 13:42:34 +0900

    MainWindowSettings 追加

---
+++ FsReport.Data/MainWindowSettings.cs
@@ -0,0 +1,17 @@
+﻿namespace FsReport.Data
+{
+    public class MainWindowSettings
+    {
+        public double MainWindowWidth { get; set; }
+
+        public double MainWindowHeight { get; set; }
+
+        public int ReportTypeIndex { get; set; }
+
+        public string? ReportFileName { get; set; }
+
+        public bool AlwaysOnTop { get; set; }
+
+        public bool AutoOpenReportFile { get; set; }
+    }
+}
